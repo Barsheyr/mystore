326f77631ab92cfbc485790370833c4a821c81a4
Bashir Amokomowo <barsheyr@gmail.com>
2024-10-07 15:06:06 +0100

    new fix

--- components/single-products/AddToCart.tsx
+++ components/single-products/AddToCart.tsx
@@ -1,10 +1,18 @@
-import { Button } from "../ui/button";
+// import { Button } from "../ui/button";
 
-function AddToCart({ productId }: { productId: string }) {
-  return (
-    <Button className="capitalize mt-8" size="lg">
-      add to cart
-    </Button>
-  );
-}
+// function AddToCart({ productId }: { productId: string }) {
+//   return (
+//     <Button className="capitalize mt-8" size="lg">
+//       add to cart
+//     </Button>
+//   );
+// }
+// export default AddToCart;
+
+import React from "react";
+
+const AddToCart = () => {
+  return <div>AddToCart</div>;
+};
+
 export default AddToCart;
